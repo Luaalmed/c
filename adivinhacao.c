fca81e703a67db1aeabd7e3507a4eb42069e81f5
Luana Almeida <134015897+Luaalmed@users.noreply.github.com>
2024-03-21 19:35:26 +0000

    implementando looping para limitar tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,39 +1,35 @@
-#include <stdio.h>
 #include <locale.h>
-#include <windows.h>
+#include <stdio.h>
 
-
-//imprime cabeçalho do jogo
+// imprime cabeçalho do jogo
 int main() {
-    printf("******************************************\n");
-    printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
-    printf("******************************************\n");
-    // váriavel
-   int numerosecreto = 42;
+  printf("******************************************\n");
+  printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
+  printf("******************************************\n");
+  // váriavel
+  int numerosecreto = 42;
 
-   int chute;
+  int chute;
+  for (int i = 1; i <= 3; i++) {
+    printf("Tentativa %d de 3\n", i);
     printf("Qual é o seu chute? ");
-    // scanf é para escrever o chute
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
 
-    int acertou = (chute==numerosecreto);
-    if (acertou){
-         printf("Parabéns, você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
+    if (scanf("%d", &chute) != 1) {
+      printf("Seu chute foi %d\n", chute);
     }
-    else {
-        printf("Você errou!\n");
-        printf("Mas não desanime, tente de novo!\n");
+    int acertou = (chute == numerosecreto);
 
-        int maior = chute > numerosecreto;
-        if(maior) {
-             printf("Seu chute foi maior que o número secreto\n");
-        
-
-        } else {
-             printf("Seu chute foi menor que o número secreto\n");
-        }
+    if (acertou) {
+      printf("Parabéns! Você acertou!\n");
+      printf("Jogue de novo, você é um bom jogador!\n");
+    } else {
+      int maior = chute > numerosecreto;
+      if (maior) {
+        printf("Seu chute foi maior que o número secreto\n");
+      } else {
+        printf("Seu chute foi menor que o número secreto\n");
+      }
     }
+  }
+  printf("Fim de jogo\n");
 }
-   
